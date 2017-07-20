1a077450609b03589ff08be2534bbbeece18d55b
Ali İmran ÖZBATMAN <aliimranozbatman@ali-macbook-pro.mynet>
2017-07-21 02:35:36 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
-
+const {ObjectID} =require("mongodb");
 var {mongoose} = require("./db/mongoose.js");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
@@ -33,6 +32,35 @@ app.get("/todos",(req,res) =>{
    })
 });
 
+//GET /todos/id like this
+
+
+app.get("/todos/:id",(req,res) =>{
+    var id=req.params.id;
+
+     if(!ObjectID.isValid(id)){
+        return res.status(404).send("Invalid id");
+        }
+    Todo.findById(id).then((todo) =>{
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+            res.status(200).send({todo});
+    }).catch((e) => res.status(400).send());
+
+    //Valid id using isValid
+    // 404 - send back empty body
+
+
+    //findById
+    //success
+    //if todo - send it back
+    //if no todo - send back 404 with empty body
+    //error
+    //400 - and send empty body back
+
+});
 
 app.listen(3000,() =>{
     console.log("Started on port 3000");
