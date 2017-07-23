2f22ce4c5563604cedc079e26abad907c937c810
Ali İmran ÖZBATMAN <aliimranozbatman@ali-macbook-pro.mynet>
2017-07-23 20:02:33 +0300

    Add GET /users/me and auth middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const {ObjectID} =require("mongodb");
 var {mongoose} = require("./db/mongoose.js");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
+var {authenticate} = require("./middleware/authenticate");
 
 
 var app=express();
@@ -133,6 +134,11 @@ app.post("/users",(req,res) =>{
     }).catch((e) =>{
         res.status(400).send(e);
     })
+
+});
+
+app.get("/users/me",authenticate,(req,res) => {
+  res.send(req.user);
 
 });
 
