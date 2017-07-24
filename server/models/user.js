474fb6cf8f90f49274622390203e09de93fd0300
Ali İmran ÖZBATMAN <aliimranozbatman@ali-macbook-pro.mynet>
2017-07-25 02:35:06 +0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -56,6 +56,18 @@ UserSchema.methods.generateAuthToken = function () {
 
 };
 
+UserSchema.methods.removeToken= function (token) {
+    var user=this;
+
+   return user.update({
+        $pull:{ //mongodb operator
+            tokens:{token}
+        }
+    });
+
+
+};
+
 UserSchema.statics.findByToken = function (token) {
   var User=this;
   var decoded;
