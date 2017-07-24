72fba23cbf4ea7283eee3d1423e1ee531ec25e77
Ali İmran ÖZBATMAN <aliimranozbatman@ali-macbook-pro.mynet>
2017-07-25 01:57:17 +0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -76,6 +76,31 @@ UserSchema.statics.findByToken = function (token) {
 
 };
 
+UserSchema.statics.findByCredentials=function (email,password){
+    var User = this;
+
+   return User.findOne({email}).then((user) => {
+      if(!user){
+          return Promise.reject(); //direk catche gidecek
+      }
+      return new Promise((resolve,reject) => {
+        //use bcrypt.compare to compare password and user.password
+
+          bcrypt.compare(password,user.password,(err,res) => {
+              if(res){
+                  resolve(user);
+              }
+              else{
+                  reject(err);
+              }
+          });
+
+      });
+   });
+
+};
+
+
 UserSchema.pre("save",function (next) {
     var user=this;
 
