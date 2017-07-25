cbca3ebf007cbcb9e095795442235e1ee4ac284e
Ali İmran ÖZBATMAN <aliimranozbatman@ali-macbook-pro.mynet>
2017-07-25 04:29:49 +0300

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,16 @@
 var env = process.env.NODE_ENV || "development";
 
+if(env === "development" || env === "test"){
+    var config = require("./config.json");//parse etmeye gerek yok json objesi
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
+
+}
+
+/*
 if(env==="development"){
     process.env.PORT=3000;
     process.env.MONGODB_URI ="mongodb://localhost:27017/TodoApp";
@@ -7,4 +18,4 @@ if(env==="development"){
     process.env.PORT=3000;
     process.env.MONGODB_URI ="mongodb://localhost:27017/TodoAppTest";
 }
-
+*/
